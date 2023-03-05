5995ab8df5be22a36405e6248dd07b7b340c4789
bright11maker <brightamponsah11@gmail.com>
2023-03-05 13:49:39 +0000

    Delete and Edit form updated

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\listingController;
 use App\Models\listing;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\listingController;
 
 
 
@@ -17,15 +18,43 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/* 
+//Common Resources Routes:
+// index - Show all listings
+// show - show single listing
+// create - show form to create new listing
+// store - store new listing 
+// edit - show form to edit listing
+// update - update listing
+// destroy - delete listing
+
+*/
+
+
 //All listings
 Route::get('/',[listingController::class, 'index']);
 
 //Show Create Form
 Route::get('/listings/create', [listingController::class, 'create']);
 
+//Store listing Data
+Route::post('/listings',[listingController::class, 'store']);
+
+//show Edit Form
+Route::get('/listings/{listing}/edit', [listingController::class, 'edit']);
+
+//Update Listing
+Route::put('/listings/{listing}', [listingController::class, 'update']);
+
+//Delete Listing
+Route::delete('/listings/{listing}', [listingController::class, 'destroy']);
 
 //Single listing
 Route::get('/listings/{listing}',[listingController::class, 'show']);
 
-//Store listing
-Route::post('/listings',[listingController::class, 'store']);
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
